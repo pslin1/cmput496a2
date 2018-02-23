842f5019d9b131433a2e4666eff8fef54b1baa29
dkotowic <kotowicz@ualberta.ca>
2018-02-22 18:52:10 -0700

    remove some extra statements and comments

--- Go2/Go2.py
+++ Go2/Go2.py
@@ -27,13 +27,10 @@ class Go2():
         self.version = 0.1
 
     def get_move(self,board, color):
-        # unsure of the return values expected from the connection
         can_win, move_to_take = self.con.solve_cmd()
         if can_win:
-            print("win")
             return move_to_take
         else:
-            print("random")
             return GoBoardUtil.generate_random_move(board,color,True)
     
 
